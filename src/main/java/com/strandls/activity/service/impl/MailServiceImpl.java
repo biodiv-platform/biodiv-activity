281630bfe975572cc675405674704fec65d2b7c7
teamdraftbox <teamdraftbox@gmail.com>
2022-05-30 10:58:38 +0530

    🐛 comment mail bug fix

--- src/main/java/com/strandls/activity/service/impl/MailServiceImpl.java
+++ src/main/java/com/strandls/activity/service/impl/MailServiceImpl.java
@@ -114,7 +114,7 @@ public class MailServiceImpl implements MailService {
 					recipient.setId(follower.getId());
 					recipient.setIsSubscribed(follower.getSendNotification());
 					data = prepareMailData(type, recipient, follower, who, reco, userGroup, activity, comment, name,
-							observation, groups, linkTaggedUsers==null && linkTaggedUsers.isEmpty() ? comment.getBody() : linkTaggedUsers,
+							observation, groups, linkTaggedUsers==null || linkTaggedUsers.isEmpty() ? comment.getBody() : linkTaggedUsers,
 							document);
 					if (follower.getEmail() != null && !follower.getEmail().isEmpty()) {
 						mailDataList.add(data);
@@ -124,7 +124,7 @@ public class MailServiceImpl implements MailService {
 				for (Recipients recipient : recipientsList) {
 					User follower = userService.getUser(String.valueOf(recipient.getId()));
 					data = prepareMailData(type, recipient, follower, who, reco, userGroup, activity, comment, name,
-							observation, groups, linkTaggedUsers==null &&linkTaggedUsers.isEmpty() ? comment.getBody() : linkTaggedUsers,
+							observation, groups, linkTaggedUsers==null ||linkTaggedUsers.isEmpty() ? comment.getBody() : linkTaggedUsers,
 							document);
 					if (recipient.getEmail() != null && !recipient.getEmail().isEmpty()) {
 						mailDataList.add(data);
